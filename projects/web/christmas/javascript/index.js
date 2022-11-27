f3e59765f1a834dbe906686dee2867bf0b57bb4b
kanmj1203 <rkdalswl1203@g.shingu.ac.kr>
2022-11-27 16:47:28 +0900

    Updates

---
+++ projects/web/christmas/javascript/index.js
@@ -0,0 +1,159 @@
+function onClicked(buttonNum)
+{
+    var targetIframe = document.getElementById("iframeId");
+    var targetIframeimg = document.getElementById("iframeIdimg");
+
+
+    if(buttonNum == 0)
+    {
+        targetIframe.src = "000.html";
+    }
+    else if (buttonNum == 1)
+    {
+        targetIframe.src = "001.html";
+    }
+    else if (buttonNum == 2)
+    {
+        targetIframe.src = "002.html";
+    }
+    else if (buttonNum == 3)
+    {
+        targetIframe.src = "003.html";
+    }
+    else if (buttonNum == 4)
+    {
+        targetIframe.src = "004.html";
+    }
+    else if (buttonNum == 5)
+    {
+        targetIframe.src = "005.html";
+    }
+
+
+
+    else if (buttonNum == 6)
+    {
+        targetIframeimg.src = "006.html";
+    }
+    else if (buttonNum == 7)
+    {
+        targetIframeimg.src = "007.html";
+    }
+    else if (buttonNum == 8)
+    {
+        targetIframeimg.src = "008.html";
+    }
+    else if (buttonNum == 9)
+    {
+        targetIframeimg.src = "009.html";
+    }
+    else if (buttonNum == 10)
+    {
+        targetIframeimg.src = "010.html";
+    }
+    else if (buttonNum == 11)
+    {
+        targetIframeimg.src = "011.html";
+    }
+    else if (buttonNum == 12)
+    {
+        targetIframeimg.src = "012.html";
+    }
+    else if (buttonNum == 13)
+    {
+        targetIframeimg.src = "013.html";
+    }
+
+
+
+    else if (buttonNum == 14)
+    {
+        targetIframeimg.src = "014.html";
+    }
+    else if (buttonNum == 15)
+    {
+        targetIframeimg.src = "015.html";
+    }
+    else if (buttonNum == 16)
+    {
+        targetIframeimg.src = "016.html";
+    }
+    else if (buttonNum == 17)
+    {
+        targetIframeimg.src = "017.html";
+    }
+    else if (buttonNum == 18)
+    {
+        targetIframeimg.src = "018.html";
+    }
+
+
+
+    else if (buttonNum == 19)
+    {
+        targetIframeimg.src = "019.html";
+    }
+    else if (buttonNum == 20)
+    {
+        targetIframeimg.src = "020.html";
+    }
+    else if (buttonNum == 21)
+    {
+        targetIframeimg.src = "021.html";
+    }
+    else if (buttonNum == 22)
+    {
+        targetIframeimg.src = "022.html";
+    }
+    else if (buttonNum == 23)
+    {
+        targetIframeimg.src = "023.html";
+    }
+    else if (buttonNum == 24)
+    {
+        targetIframeimg.src = "024.html";
+    }
+
+}
+
+function home_onMouseOver(DOM)
+{
+    DOM.style.fontSize = "44px";
+    DOM.a_opacity = "50";
+    DOM.a_time = "300";
+}
+
+function home_onMouseOut(DOM)
+{
+    DOM.style.fontSize = "40px";
+    DOM.a_opacity = "50";
+    DOM.a_time = "300";
+}
+
+
+
+
+
+function topmenu_onMouseOver(DOM)
+{
+    DOM.style.fontSize = "24px";
+    DOM.a_opacity = "50";
+    DOM.a_time = "300";
+}
+
+function topmenu_onMouseOut(DOM)
+{
+    DOM.style.fontSize = "20px";
+}
+
+function mainImage01_onMouseOver(DOM)
+{
+    DOM.style.width = "105px";
+    DOM.style.height= "135px";
+}
+
+function mainImage01_onMouseOut(DOM)
+{
+    DOM.style.width = "100px";
+    DOM.style.height= "130px";
+}
